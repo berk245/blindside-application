5c8c5dbb3be14ede040bc90fddae30688ca76dfb
berk245 <berkozzambak@gmail.com>
2022-04-28 15:30:26 +0200

    Update watch video section with basic layout and loading./error handling

--- src/routes/WatchVideo.jsx
+++ src/routes/WatchVideo.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 function WatchVideo() {
+
+  const [fetchingData, setFetchingData] = useState(true);
+  const [fetchError, setFetchError] = useState(false);
+  const [videoInfo, setVideoInfo] = useState({});
+
   return (
-    <div>WatchVideo</div>
+    <div>
+    {!fetchingData ? (
+      <p style={{ textAlign: "center", padding: "5rem 0" }}>Loading</p>
+    ) : (
+      <>
+        {!fetchError ? (
+          <div className="watch-video-main-container">
+            <Link to={{ pathname: "/" }}>
+              <button className="button">Go back</button>
+            </Link>
+            <div className="watch-video-content">
+              <div className="left-section">
+                <div className="">Video Player</div>
+                <div className="">Comments</div>
+              </div>
+
+              <div className="related-videos">
+                Related Vids
+              </div>
+            </div>
+          </div>
+        ) : (
+          <p>
+            An error occured while accessing data. Please refresh the page.
+          </p>
+        )}
+      </>
+    )}
+  </div>
   )
 }
 
