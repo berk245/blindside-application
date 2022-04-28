07ace2efcdb0b8783bf2b4174305eb9524a9a590
berk245 <berkozzambak@gmail.com>
2022-04-28 15:41:05 +0200

    Add videoId parser and get video id functions

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,30 @@
-import {dataSet} from './static/sampleData.js'
+import { dataSet } from "./static/sampleData.js";
 
 const sleep = (ms) => {
-    return new Promise(resolve => {setTimeout(resolve, ms);})
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+const getUserVideos = async () => {
+  await sleep(3000);
+  return dataSet;
+};
+
+const getVideoId = (url) => {
+  try {
+    let splitted = url.split("/");
+    let result = splitted[splitted.length - 1];
+    return result;
+  } catch {
+    return false;
   }
+};
 
-  const getUserVideos = async() => {
-    await sleep(3000)
-    return dataSet
+const getVideoInfo = async (id) => {
+  console.log(id);
+  await sleep(1000);
+  return dataSet[0];
+};
 
-}
-
-export {getUserVideos}
+export { getUserVideos, getVideoId, getVideoInfo };
