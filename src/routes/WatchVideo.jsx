e38d910bf1974439dbd35922ddf920d760e549ac
berk245 <berkozzambak@gmail.com>
2022-04-28 15:56:19 +0200

    Import Video Player component in watch video view nad feed it the url and video info

--- src/routes/WatchVideo.jsx
+++ src/routes/WatchVideo.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import {getVideoId, getVideoInfo} from '../utils'
+import VideoPlayer from "../components/VideoPlayer/VideoPlayer";
+import { getVideoId, getVideoInfo } from "../utils";
 function WatchVideo() {
-
   const [fetchingData, setFetchingData] = useState(true);
   const [fetchError, setFetchError] = useState(false);
   const [videoInfo, setVideoInfo] = useState({});
@@ -21,35 +21,34 @@ function WatchVideo() {
   }, []);
   return (
     <div>
-    {!fetchingData ? (
-      <p style={{ textAlign: "center", padding: "5rem 0" }}>Loading</p>
-    ) : (
-      <>
-        {!fetchError ? (
-          <div className="watch-video-main-container">
-            <Link to={{ pathname: "/" }}>
-              <button className="button">Go back</button>
-            </Link>
-            <div className="watch-video-content">
-              <div className="left-section">
-                <div className="">Video Player</div>
-                <div className="">Comments</div>
-              </div>
+      {fetchingData ? (
+        <p style={{ textAlign: "center", padding: "5rem 0" }}>Loading</p>
+      ) : (
+        <>
+          {!fetchError ? (
+            <div className="watch-video-main-container">
+              <Link to={{ pathname: "/" }}>
+                <button className="button">Go back</button>
+              </Link>
+              <div className="watch-video-content">
+                <div className="left-section">
+                  <VideoPlayer className="video-player" video={videoInfo} />
 
-              <div className="related-videos">
-                Related Vids
+                  <div className="">Comments</div>
+                </div>
+
+                <div className="related-videos">Related Vids</div>
               </div>
             </div>
-          </div>
-        ) : (
-          <p>
-            An error occured while accessing data. Please refresh the page.
-          </p>
-        )}
-      </>
-    )}
-  </div>
-  )
+          ) : (
+            <p>
+              An error occured while accessing data. Please refresh the page.
+            </p>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default WatchVideo
+export default WatchVideo;
