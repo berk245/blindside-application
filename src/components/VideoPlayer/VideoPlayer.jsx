044d4184d5878a37215d4e78b374763fc032d7f7
berk245 <berkozzambak@gmail.com>
2022-04-28 15:55:00 +0200

    Add a basic video player component with react-player library

---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+function VideoPlayer({video}) {
+
+  let {videoUrl, videoTitle, postedAt} = video
+  return (
+    <>
+    <ReactPlayer className='video-player' controls url={videoUrl}></ReactPlayer>
+    <div className='video-player-info-section'>
+    <span style={{fontWeight: 600}}>{videoTitle}</span>
+    <span>{postedAt}</span>
+    </div>
+    </>
+ 
+  )
+}
+
+export default VideoPlayer
